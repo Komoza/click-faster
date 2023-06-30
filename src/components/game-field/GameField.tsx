f1ab8e34c997b0d4ce62f5f6a77fea3a53a1c946
Maxim <MaximKomoza@yandex.ru>
2023-06-30 11:14:51 +0700

    click animation && point animation

--- src/components/game-field/GameField.tsx
+++ src/components/game-field/GameField.tsx
@@ -5,24 +5,60 @@ interface GameFieldProps {
     setCounter: React.Dispatch<React.SetStateAction<number>>;
     startTimer: () => void;
 }
+
+interface Point {
+    x: number | null;
+    y: number | null;
+}
+interface Click {
+    x: number | null;
+    y: number | null;
+}
 export const GameField: React.FC<GameFieldProps> = ({
     startTimer,
     counter,
     setCounter,
 }) => {
     const sizePoint: number = 100;
-    interface Point {
-        x: number | null;
-        y: number | null;
-    }
 
     const [isStarted, setIsStarted] = useState<boolean>(false);
     const [isMiss, setIsMiss] = useState<boolean>(false);
-
     const [point, setPoint] = useState<Point>({ x: null, y: null });
+
+    const [isClick, setIsClick] = useState<boolean>(false);
+    const [clickAnimation, setClickAnimation] = useState<Click>({
+        x: null,
+        y: null,
+    });
+
+    const pointRef = useRef<HTMLDivElement>(null);
     const gameFieldRef = useRef<HTMLDivElement>(null);
 
-    const getRandomPoint = () => {
+    const animateClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        const { offsetX, offsetY } = event.nativeEvent;
+        setIsClick(true);
+        setClickAnimation({ x: offsetX, y: offsetY });
+
+        setTimeout (() => {
+            setIsClick(false);
+        }, 200)
+    };
+
+    const animatePoint = () => {
+        if (pointRef.current)
+            pointRef.current.classList.add('game-field__point--animation');
+
+        setTimeout(() => {
+            if (pointRef.current)
+                pointRef.current.classList.remove(
+                    'game-field__point--animation'
+                );
+        }, 200);
+    };
+
+    const setRandomPoint = () => {
         if (gameFieldRef.current) {
             const gameFieldWidth =
                 gameFieldRef.current.offsetWidth - sizePoint - 10;
@@ -40,27 +76,33 @@ export const GameField: React.FC<GameFieldProps> = ({
 
             setPoint({ x: randomX, y: randomY });
         }
+        animatePoint();
     };
-    const handleClickPoint = (
-        event?: React.MouseEvent<HTMLDivElement, MouseEvent>
-    ) => {
-        if (event) {
-            event.stopPropagation();
-        }
 
-        getRandomPoint();
+    const handleClickPoint = () => {
+        setRandomPoint();
         setCounter(counter + 1);
     };
 
     const start = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.stopPropagation();
-        getRandomPoint();
+        setRandomPoint();
         setIsStarted(true);
+        if (pointRef.current)
+            pointRef.current.classList.add('game-field__point--animation');
         startTimer();
     };
-    const mistake = () => {
-        setCounter(counter ? counter - 1: 0);
-        getRandomPoint();
+
+    const mistake = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        animateClick(event);
+        if (
+            pointRef.current &&
+            pointRef.current.contains(event.target as Node)
+        ) {
+            return;
+        }
+        setCounter(counter ? counter - 1 : 0);
+        setRandomPoint();
         setIsMiss(true);
         setTimeout(() => {
             setIsMiss(false);
@@ -68,7 +110,20 @@ export const GameField: React.FC<GameFieldProps> = ({
     };
 
     return (
-        <div ref={gameFieldRef} className="game-field" onClick={mistake}>
+        <div
+            ref={gameFieldRef}
+            className="game-field"
+            onClick={(event) => mistake(event)}
+        >
+            {isClick && (
+                <div
+                    style={{
+                        top: `${clickAnimation.y}px`,
+                        left: `${clickAnimation.x}px`,
+                    }}
+                    className="game-field--click"
+                ></div>
+            )}
             {isMiss && <div className="game-field__miss-screen"></div>}
             {!isStarted && (
                 <div
@@ -83,18 +138,17 @@ export const GameField: React.FC<GameFieldProps> = ({
                     </button>
                 </div>
             )}
-            {point.x !== null && point.y !== null && (
-                <div
-                    className="game-field__point"
-                    onClick={(event) => handleClickPoint(event)}
-                    style={{
-                        width: `${sizePoint}px`,
-                        height: `${sizePoint}px`,
-                        top: `${point.y}px`,
-                        left: `${point.x}px`,
-                    }}
-                ></div>
-            )}
+            <div
+                ref={pointRef}
+                className="game-field__point"
+                onClick={handleClickPoint}
+                style={{
+                    width: `${!point.x || !point.y ? 0 : sizePoint}px`,
+                    height: `${sizePoint}px`,
+                    top: `${point.y}px`,
+                    left: `${point.x}px`,
+                }}
+            ></div>
         </div>
     );
 };
